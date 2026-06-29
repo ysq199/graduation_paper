b4a6b3f943ee2474a7c1c03f4d9bb991c76ab105
Sq y <1251223192@qq.com>
2026-06-29 09:58:55 +0800

    updated the training results for yolov8n

--- yolo/yolov11/main.py
+++ yolo/yolov11/main.py
@@ -3,11 +3,10 @@ from ultralytics import YOLO
 DATA_CONF_PATH = r"D:\projects\graduation_paper\yolo\datasets\severstal-steel-defect-instance-segmentation.v4i.yolov11\data.yaml"
 EPOCHS = 200
 DEVICE = 0
-MODEL_CFG = r"yolo11-seg-p2.yaml"   # P2 分支模型配置
-PRETRAINED = "yolo11s-seg.pt"               # 用官方 seg 预训练权重初始化
+
 def main():
     # 从 YAML 创建模型结构，加载预训练权重
-    model = YOLO(MODEL_CFG).load(PRETRAINED)
+    model = YOLO("yolopavv8n-seg.pt")
 
     model.train(
         data=DATA_CONF_PATH,
@@ -22,7 +21,7 @@ def main():
         cache="disk",        # 比 cache=True 更稳，少吃内存
         amp=True,
 
-        name="yolo11s-seg-p2_runs",
+        name="yolov8n-seg_runs",
         patience=50,
         save=True,
 
